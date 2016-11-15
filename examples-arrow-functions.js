0a3edd6ede8f761ede63982bb7d25ca85ac51a80
JP <phillemieux66@hotmail.com>
2016-11-14 19:56:37 -0500

    Init Commit

---
+++ examples-arrow-functions.js
@@ -0,0 +1,29 @@
+// var names = ['Andrew','Julie','Jen'];
+
+// names.forEach(function(name) {
+//   console.log('forEach',name);
+// });
+//
+// names.forEach((name) => {
+//   console.log('arrowFunc', name);
+// });
+//
+// names.forEach((name) => console.log(name));
+//
+// var returnMe = (name) => name + '!';
+// console.log(returnMe('JP'));
+//
+// var person = {
+//   name: 'JP',
+//   greet: function(){
+//     names.forEach((name) => {
+//       console.log(this.name + 'says hi to ' + name);
+//     });
+//   }
+// };
+//
+// person.greet();
+
+function add(a,b) {
+  return a + b;
+}
