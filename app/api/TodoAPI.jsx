d95691f9732f462d39234034a61bb3056fd3a300
JP <phillemieux66@hotmail.com>
2016-11-30 16:13:00 -0500

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,35 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      if (searchText === '' || todo.text.toLowerCase().indexOf(searchText) != -1) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    //Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed){
+        return - 1;
+      } else if (a.completed && !b.completed){
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
